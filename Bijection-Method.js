14e78744218113031055048da50f408dcb98192c
PriyanshuShekhar10 <62722200+PriyanshuShekhar10@users.noreply.github.com>
2023-08-18 15:32:37 +0530

    fix Bijection code

--- Bijection-Method.js
+++ Bijection-Method.js
@@ -6,40 +6,36 @@ const readline = require("readline").createInterface({
 const math = require("mathjs");
 
 let a, b, c, e, Root;
-readline.question("Input Left limit guess", (x_l) => {
+readline.question("Input Left limit guess: ", (x_l) => {
   a = parseFloat(x_l);
-  readline.question("Input Right limit guess", (x_r) => {
+  readline.question("Input Right limit guess: ", (x_r) => {
     c = parseFloat(x_r);
-    readline.question("Enter the error limit", (error) => {
+    readline.question("Enter the error limit: ", (error) => {
       e = parseFloat(error);
       readline.close();
-      console.log(` Left guess : ${a}, Right guess : ${c}, Error Limit: ${e}`);
-      performBijection();
+      console.log(`Left guess: ${a}, Right guess: ${c}, Error Limit: ${e}`);
+      performBisection();
     });
   });
 });
 
-const GivenFunction = (x) => {
+const givenFunction = (x) => {
   return math.cube(x) - 4 * x - 9;
 };
 
-const bisectInterval = () => {
+const performBisection = () => {
   b = (a + c) / 2;
-  performBijection();
-};
-
-const performBijection = () => {
-  if (GivenFunction(a) * GivenFunction(b) < 0) {
-    c = b;
-  } else {
-    a = b;
-  }
-  checkConvergence();
-};
-
-const checkConvergence = () => {
-  if (math.abs(a - c) < e) {
+  if (Math.abs(a - c) < e) {
     Root = b;
-    console.log(`The final root is : ${Root}`);
-  } else bisectInterval();
+    console.log(`The final root is: ${Root}`);
+  } else {
+    const functionAtA = givenFunction(a);
+    const functionAtB = givenFunction(b);
+    if (functionAtA * functionAtB < 0) {
+      c = b;
+    } else {
+      a = b;
+    }
+    performBisection();
+  }
 };
