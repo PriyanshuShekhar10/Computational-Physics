313a070fd27aa18161ea42c3b5320c169d89fdf3
PriyanshuShekhar10 <62722200+PriyanshuShekhar10@users.noreply.github.com>
2023-08-22 22:14:31 +0530

    fix sum argument

--- Numerical-Integration/trapezoidal_rule.js
+++ Numerical-Integration/trapezoidal_rule.js
@@ -1,7 +1,7 @@
 const math = require("mathjs");
 
 const f_x = (x) => {
-  return x * (math.e * (-1 * x));
+  return x * Math.exp(-1 * x);
 };
 
 let lower_bound = 0.2;
@@ -9,14 +9,15 @@ let upper_bound = 1.4;
 let integral = [];
 
 let number_of_steps = 2;
-let step_size = (upper_bound - lower_bound) / number_of_steps;
 
 const integration = () => {
+  let step_size = (upper_bound - lower_bound) / number_of_steps;
+
   const common_sum = step_size * (f_x(lower_bound) / 2 + f_x(upper_bound) / 2);
   const variable_sum = () => {
     let sum = 0;
     for (let i = 1; i < number_of_steps; i++) {
-      sum = sum + f_x(i * step_size);
+      sum = sum + f_x(lower_bound + i * step_size);
     }
     return sum;
   };
@@ -26,7 +27,7 @@ const integration = () => {
 
 const step_increment = () => {
   let x = 0;
-  while (x < 1000) {
+  while (x < 100) {
     console.log(`Integral for Step ${x + 1}: ${integration()}`);
     x++;
     number_of_steps++;
